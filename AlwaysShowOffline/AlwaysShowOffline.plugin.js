8e65f0b2984475b454c48b7ec82e56034faf83f5
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2021-10-25 16:51:13 +1100

    Update AlwaysShowOffline.plugin.js

--- AlwaysShowOffline/AlwaysShowOffline.plugin.js
+++ AlwaysShowOffline/AlwaysShowOffline.plugin.js
@@ -2,7 +2,7 @@
  * @name AlwaysShowOffline
  * @author TheCommieAxolotl#6898
  * @description Always show offline users in memberlist.
- * @version 0.0.6
+ * @version 0.0.5
  * @authorId 538487970408300544
  * @source https://raw.githubusercontent.com/TheCommieAxolotl/BetterDiscord-Stuff/main/AlwaysShowOffline/AlwaysShowOffline.plugin.js
  * @updateurl https://raw.githubusercontent.com/TheCommieAxolotl/BetterDiscord-Stuff/main/AlwaysShowOffline/AlwaysShowOffline.plugin.js
@@ -23,7 +23,7 @@ module.exports = (() => {
                 }
             ],
             github_raw: "https://raw.githubusercontent.com/TheCommieAxolotl/BetterDiscord-Stuff/main/AlwaysShowOffline/AlwaysShowOffline.plugin.js",
-            version: "0.0.6",
+            version: "0.0.5",
             description: "Always show offline users in memberlist. (Disabling requires restart)"
         },
 
