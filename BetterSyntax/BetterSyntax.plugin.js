fc4e7e023574dbc1e17bfe103ce1aa9e0619db52
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-02-26 10:57:51 +1100

    add toast response

--- BetterSyntax/BetterSyntax.plugin.js
+++ BetterSyntax/BetterSyntax.plugin.js
@@ -421,6 +421,7 @@ module.exports = (() => {
                               ele.addEventListener("click", async () => {
                                   const text = ele.parentElement.parentElement.textContent;
                                   DiscordNative.clipboard.copy(text);
+                                  BdApi.showToast("Copied", {type: "success"})
                               });
                           }
 
