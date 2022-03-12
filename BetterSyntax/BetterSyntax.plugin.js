b2bd70c5bd69dac33ecb92b29aac6346012f522b
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-03-13 10:49:35 +1100

    Zlib Alert

--- BetterSyntax/BetterSyntax.plugin.js
+++ BetterSyntax/BetterSyntax.plugin.js
@@ -243,6 +243,18 @@ module.exports = (() => {
               getVersion() {
                   return config.info.version;
               }
+              load() {
+                  BdApi.showConfirmationModal("Library Missing", `The library plugin needed for **${config.info.name}** is missing. Please click Download Now to install it.`, {
+                      confirmText: "Download Now",
+                      cancelText: "Cancel",
+                      onConfirm: () => {
+                          require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                              if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                              await new Promise((r) => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                          });
+                      },
+                  });
+              }
               start() {}
               stop() {}
           }
