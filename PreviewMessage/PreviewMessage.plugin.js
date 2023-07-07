2f0fcaa7efe6e25ca5f1a91ea3ab3faf44727a8b
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-07-07 11:42:35 +1000

    Fix button positioning - PreviewMessage

--- PreviewMessage/PreviewMessage.plugin.js
+++ PreviewMessage/PreviewMessage.plugin.js
@@ -82,9 +82,7 @@ module.exports = (() => {
 .preview-button {
     background-color: transparent;
     border: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    height: 8px;
     filter: grayscale(100%);
     cursor: pointer;
     color: var(--interactive-normal);
@@ -111,17 +109,14 @@ module.exports = (() => {
                           injectCSS("PreviewMessage-Styles", Styles);
 
                           Patcher.after(ChannelTextArea.type, "render", (_, __, ret) => {
-                              const chatBar = Utilities.findInReactTree(
-                                  ret,
-                                  (n) => Array.isArray(n?.children) && n.children.some((c) => c?.props?.className?.startsWith("attachButton"))
-                              )?.children;
+                              const chatBar = Utilities.findInReactTree(ret, (n) => Array.isArray(n?.children) && n.children.some((c) => c?.props?.className?.startsWith("attachButton")))?.children;
                               if (!chatBar) {
                                   console.error("PreviewMessage: Couldn't find ChatBar component in React tree");
                                   return;
                               }
+
                               const buttons = Utilities.findInReactTree(chatBar, (n) => n?.props?.showCharacterCount);
-                              if (buttons?.props.disabled)
-                                  return;
+                              if (buttons?.props.disabled) return;
 
                               chatBar.splice(-1, 0, this.renderButton());
                           });
@@ -142,80 +137,88 @@ module.exports = (() => {
                               text: "Preview Message",
                               children: (p) =>
                                   React.createElement(
-                                      "button",
+                                      "div",
                                       {
-                                          ...p,
-                                          className: "preview-button",
-                                          onClick: () => {
-                                              this.sendPreview();
+                                          style: {
+                                              marginTop: "10px",
                                           },
                                       },
                                       React.createElement(
-                                          "svg",
+                                          "button",
                                           {
-                                              xmlns: "http://www.w3.org/2000/svg",
-                                              viewBox: "0 0 36 36",
-                                              width: "24",
-                                              height: "24",
+                                              ...p,
+                                              className: "preview-button",
+                                              onClick: () => {
+                                                  this.sendPreview();
+                                              },
                                           },
-                                          React.createElement("ellipse", {
-                                              fill: "currentColor",
-                                              cx: "8.828",
-                                              cy: "18",
-                                              rx: "7.953",
-                                              ry: "13.281",
-                                          }),
-                                          React.createElement("path", {
-                                              fill: "currentColor",
-                                              d: "M8.828 32.031C3.948 32.031.125 25.868.125 18S3.948 3.969 8.828 3.969 17.531 10.132 17.531 18s-3.823 14.031-8.703 14.031zm0-26.562C4.856 5.469 1.625 11.09 1.625 18s3.231 12.531 7.203 12.531S16.031 24.91 16.031 18 12.8 5.469 8.828 5.469z",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
-                                              cx: "6.594",
-                                              cy: "18",
-                                              r: "4.96",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
-                                              cx: "6.594",
-                                              cy: "18",
-                                              r: "3.565",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "currentColor",
-                                              cx: "7.911",
-                                              cy: "15.443",
-                                              r: "1.426",
-                                          }),
-                                          React.createElement("ellipse", {
-                                              fill: "currentColor",
-                                              cx: "27.234",
-                                              cy: "18",
-                                              rx: "7.953",
-                                              ry: "13.281",
-                                          }),
-                                          React.createElement("path", {
-                                              fill: "currentColor",
-                                              d: "M27.234 32.031c-4.88 0-8.703-6.163-8.703-14.031s3.823-14.031 8.703-14.031S35.938 10.132 35.938 18s-3.824 14.031-8.704 14.031zm0-26.562c-3.972 0-7.203 5.622-7.203 12.531 0 6.91 3.231 12.531 7.203 12.531S34.438 24.91 34.438 18 31.206 5.469 27.234 5.469z",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
-                                              cx: "25",
-                                              cy: "18",
-                                              r: "4.96",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
-                                              cx: "25",
-                                              cy: "18",
-                                              r: "3.565",
-                                          }),
-                                          React.createElement("circle", {
-                                              fill: "currentColor",
-                                              cx: "26.317",
-                                              cy: "15.443",
-                                              r: "1.426",
-                                          })
+                                          React.createElement(
+                                              "svg",
+                                              {
+                                                  xmlns: "http://www.w3.org/2000/svg",
+                                                  viewBox: "0 0 36 36",
+                                                  width: "24",
+                                                  height: "24",
+                                              },
+                                              React.createElement("ellipse", {
+                                                  fill: "currentColor",
+                                                  cx: "8.828",
+                                                  cy: "18",
+                                                  rx: "7.953",
+                                                  ry: "13.281",
+                                              }),
+                                              React.createElement("path", {
+                                                  fill: "currentColor",
+                                                  d: "M8.828 32.031C3.948 32.031.125 25.868.125 18S3.948 3.969 8.828 3.969 17.531 10.132 17.531 18s-3.823 14.031-8.703 14.031zm0-26.562C4.856 5.469 1.625 11.09 1.625 18s3.231 12.531 7.203 12.531S16.031 24.91 16.031 18 12.8 5.469 8.828 5.469z",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
+                                                  cx: "6.594",
+                                                  cy: "18",
+                                                  r: "4.96",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
+                                                  cx: "6.594",
+                                                  cy: "18",
+                                                  r: "3.565",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "currentColor",
+                                                  cx: "7.911",
+                                                  cy: "15.443",
+                                                  r: "1.426",
+                                              }),
+                                              React.createElement("ellipse", {
+                                                  fill: "currentColor",
+                                                  cx: "27.234",
+                                                  cy: "18",
+                                                  rx: "7.953",
+                                                  ry: "13.281",
+                                              }),
+                                              React.createElement("path", {
+                                                  fill: "currentColor",
+                                                  d: "M27.234 32.031c-4.88 0-8.703-6.163-8.703-14.031s3.823-14.031 8.703-14.031S35.938 10.132 35.938 18s-3.824 14.031-8.704 14.031zm0-26.562c-3.972 0-7.203 5.622-7.203 12.531 0 6.91 3.231 12.531 7.203 12.531S34.438 24.91 34.438 18 31.206 5.469 27.234 5.469z",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
+                                                  cx: "25",
+                                                  cy: "18",
+                                                  r: "4.96",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "var(--bg-overlay-3,var(--channeltextarea-background))",
+                                                  cx: "25",
+                                                  cy: "18",
+                                                  r: "3.565",
+                                              }),
+                                              React.createElement("circle", {
+                                                  fill: "currentColor",
+                                                  cx: "26.317",
+                                                  cy: "15.443",
+                                                  r: "1.426",
+                                              })
+                                          )
                                       )
                                   ),
                           });
